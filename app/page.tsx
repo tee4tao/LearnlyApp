9c1fe682771cc85b6acd91128f6105ac0605656f
Taofeek Adeniyi <adeniyitaofeek001@gmail.com>
2025-02-14 12:56:49 +0100

    refactor: update global styles and remove unused SVG files

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,95 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    const updateNavbarHeight = () => {
+      const navbar = document.getElementById("navbar");
+      if (navbar) {
+        document.documentElement.style.setProperty(
+          "--navbar-height",
+          `${navbar.offsetHeight}px`
+        );
+      }
+    };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    updateNavbarHeight();
+    window.addEventListener("resize", updateNavbarHeight);
+
+    return () => {
+      window.removeEventListener("resize", updateNavbarHeight);
+    };
+  }, []);
+  return (
+    <div className=" w-full flex flex-col items-center  justify-center min-h-screen">
+      <nav
+        id="navbar"
+        className="bg-white w-full fixed top-0 shadow-xl  py-4 px-10 flex justify-center "
+      >
+        <div className="container flex  items-center justify-between">
+          <Image
+            src={"../icons/new-logo.svg"}
+            alt="learnly-app logo"
+            width={30}
+            height={30}
+          />
+          <button className="bg-purple-700 text-white p-2 px-4 capitalize rounded-lg font-semibold text-nowrap hover:scale-90 transition-all duration-300">
+            start leaning
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+      </nav>
+      <section className="w-full mt-[var(--navbar-height)] container px-10 flex flex-col lg:flex-row justify-center lg:items-center">
+        <div className="mb-10">
+          <h2 className=" text-3xl lg:text-5xl font-semibold mb-2">
+            The <span className="text-purple-700">Most Fun</span> Way To Learn
+          </h2>
+          <p className="[word-spacing:2px] text-[#6e6e6e]">
+            Welcome to the most thrilling and enjoyable way to learn. Level up
+            your skills with LearnlyApp&apos;s immersive gamified learning
+            experience.
+          </p>
+          <button className="bg-purple-700 text-white p-2 px-4 capitalize rounded-lg font-semibold text-nowrap hover:scale-90 transition-all duration-300 mt-8">
+            start leaning
+          </button>
+        </div>
+        <div className="self-center relative">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={"/images/laptop.gif"}
+            alt="laptop"
+            width={500}
+            height={500}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
+            src={"../icons/bootcamp.svg"}
+            alt="laptop"
+            width={80}
+            height={80}
+            className="absolute top-0 -left-4"
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+            src={"../icons/workshop.svg"}
+            alt="laptop"
+            width={80}
+            height={80}
+            className="absolute top-0 -right-4"
           />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <Image
+            src={"../icons/resources.svg"}
+            alt="laptop"
+            width={80}
+            height={80}
+            className="absolute bottom-12 -left-4"
+          />
+          <Image
+            src={"../icons/game.svg"}
+            alt="laptop"
+            width={80}
+            height={80}
+            className="absolute bottom-12 -right-4"
+          />
+        </div>
+      </section>
     </div>
   );
 }
